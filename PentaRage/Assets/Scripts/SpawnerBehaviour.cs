75a1593633d67ec6807468ec547e85ea19d203aa
stpkforce <jackson22@gmail.com>
2014-06-30 00:33:28 -0700

    added spawn targets
    spawner makes 10 targets that goto the main camera

---
+++ PentaRage/Assets/Scripts/SpawnerBehaviour.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnerBehaviour : MonoBehaviour 
+{
+    public GameObject Targets;
+    public int SpawnCounter = 10;
+    public int SpawnCoolDown = 1;
+    
+	// Use this for initialization
+	void Start () 
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        if (_timer > 0)
+        {
+            _timer -= Time.deltaTime;
+            return;
+        }
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            SpawnTarget();
+            _timer = SpawnCoolDown;
+        }
+	}
+
+    public void SpawnTarget()
+    {
+        for (var i = 0; i < SpawnCounter; i++)
+        {
+            var target = (GameObject) GameObject.Instantiate(Targets);
+            var x = Random.Range(-10, 10);
+            var z = Random.Range(-10, 10);
+            var location = transform.position;
+            location.x += x;
+            location.z += z;
+            target.transform.position = location;
+        }
+    }
+    private float _timer = 0;
+}
---
+++ PentaRage/Assets/Scripts/TargetBehaviour.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TargetBehaviour : MonoBehaviour 
+{
+    public float Speed = 0.1f;
+    public float radius = 50.0F;
+    public float power = 100.0F;
+	
+    // Use this for initialization
+	void Start () 
+    {
+        mainCamera = Camera.main;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        var direction = (mainCamera.transform.position - transform.position);
+        if (direction.sqrMagnitude < 1)
+        {
+            Detonate();
+            return;
+        }
+        direction.Normalize();
+        direction.y = 0;
+        transform.Translate(Speed * direction);
+        
+	}
+
+    public void Detonate()
+    {
+        GameObject.DestroyImmediate(gameObject);
+    }
+
+    private Camera mainCamera;
+}
